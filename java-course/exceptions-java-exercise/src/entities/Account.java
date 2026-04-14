5e53e95a2b6644424d3d700e58a265c17fdecf54
wgaabs <wtgprogramador@gmail.com>
2026-04-14 22:44:37 +0100

    feat: handle account withdrawal exceptions (section 14)

---
+++ java-course/exceptions-java-exercise/src/entities/Account.java
@@ -0,0 +1,49 @@
+package entities;
+
+public class Account {
+    private Integer number;
+    private String holder;
+    private Double balance;
+    private Double withdrawLimit;
+
+    public Account(Integer number, String holder, Double balance, Double withdrawLimit) {
+        this.number = number;
+        this.holder = holder;
+        this.balance = balance;
+        this.withdrawLimit = withdrawLimit;
+    }
+
+    public void deposit(Double amount) {
+        balance += amount;
+    }
+
+    public void withdraw(Double amount) throws Exception {
+        if (amount > withdrawLimit) {
+            throw new Exception("The amount exceeds withdraw limit");
+        }
+
+        if (amount > balance) {
+            throw new Exception("Not enough balance");
+        }
+
+        balance -= amount;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public String getHolder() {
+        return holder;
+    }
+
+    public Double getBalance() {
+        return balance;
+    }
+
+    public Double getWithdrawLimit() {
+        return withdrawLimit;
+    }
+}
+
+
